6f3233cbb2e0418e02c9eb867933d0a7905665e6
Antoine Juteau <gunterstrikedev@gmail.com>
2020-02-24 17:05:22 +0100

    trying controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'places/new'
+  get 'places/show'
+  get 'places/edit'
+  get 'places/update'
+  get 'places/create'
+  get 'places/destroy'
+  get 'places/index'
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
