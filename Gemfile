13f0c770d51eaeed674a576bc2a0cf21aa506c1d
Laura Lauret Milazzo <laura.lauret.milazzo@gmail.com>
2020-02-26 18:34:01 +0100

    ok

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '6.0.2.1'
 gem 'redis'
 gem 'devise'
 gem 'bootstrap'
+gem 'faker'
 
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.6.1'
--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,12 +1,11 @@
 class PlacesController < ApplicationController
 
   def index
-    @place = Place.all
+    @places = Place.all
   end
 
   def new
     @place = Place.new
-    redirect_to place_path(@place)
   end
 
   def show
@@ -36,7 +35,7 @@ class PlacesController < ApplicationController
 
 private
   def place_params
-    params.require(:place).permit(:category, :city, :price, :adress)
+    params.require(:place).permit(:category, :city, :price, :adress, photos: [])
   end
 
 end
