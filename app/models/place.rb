ba48ad3dab4f9b2fe4a22bea89e78bc77b70560d
Laura Lauret Milazzo <laura.lauret.milazzo@gmail.com>
2020-02-27 11:01:20 +0100

    adding seed for day 3

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,7 @@ class Place < ApplicationRecord
   belongs_to :owner, class_name: 'User', foreign_key: 'user_id' #belongs to owner, association
   validates :name, presence: true
   validates :description, presence: true
-  # validates :options, presence: true
+  validates :options, presence: true
   has_many :bookings
   validates :city, presence: true
   validates :price, presence: true
