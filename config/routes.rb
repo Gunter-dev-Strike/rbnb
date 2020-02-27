447197a20426712e7b36e10f80bc1f0cd944079f
Pat C <patiere@tutanota.com>
2020-02-27 15:40:36 +0100

    navbar and bookings links

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
+  get 'pages/my_bookings', to: "pages#my_bookings"
+  get 'pages/my_places', to: "pages#my_places"
+  devise_for :users
   get 'bookings/new'
   get 'bookings/create'
-  devise_for :users
+
   root to: 'pages#home'
   resources :places do
       resources :bookings
     end
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
